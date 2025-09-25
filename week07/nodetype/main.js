a07998fc392ac1ad55ed905fbab84e0a2e060ad6
Bearry2626 <satang3223@gmail.com>
2025-09-26 00:19:54 +0700

    update week07

--- week07/nodetype/main.js
+++ week07/nodetype/main.js
@@ -1,7 +1,7 @@
 // 093 ทัศนัยภูวดล
 // 1. What is the node type of firstChild of root node?
 console.log(document.firstChild.nodeType)
-// 2. if node type is an HTML node, display "Hello, HTML node", otherwise ,display "Hello, any node"
+// 2. if node type is an element node, display "Hello, HTML node", otherwise ,display "Hello, any node"
 if (document.firstChild.nodeType === 1) {
   console.log("Hello, HTML node")
 } else {
