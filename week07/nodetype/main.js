db56b02a0b9173fb2d380aa808633af0b91bafb8
Bearry2626 <satang3223@gmail.com>
2025-09-30 00:01:59 +0700

    update week07

--- week07/nodetype/main.js
+++ week07/nodetype/main.js
@@ -14,7 +14,33 @@ console.log(divElement.nodeName)
 console.log(divElement.nodeType)
 console.log(divElement.nodeValue) // null เพราะ element ไม่มี nodeValu
 
-const pText = divElement.childNodes[1]
+const pext = divElement.childNodes[1].childNodes[0]
 console.log(pText.nodeName)
 console.log(pText.nodeType)
 console.log(pText.nodeValue) // มี nodeValue เพราะเป็น text node
+
+// Create tag attribute text node element
+// ถ้าให้ dinamic create = ตอนWebpage load ไม่เคยมี node 
+// นี้ไม่เคยมี tag html นี้ และให้สร้างหลัง rander ไปแล้ว
+
+const newDiv = document.createElement('div') // สร้าง div element
+// กลุ่มสร้างจะเป็น createElement, createTextNode, createAttribute
+//1. create <p></p>
+const pElement = document.createElement("p") //<p></p>
+
+//2. create id="p5"
+const idAttr = document.createAttribute("id")
+idAttr.value = "p5"
+
+//3. binding id="p5" to <p>
+pElement.setAttributeNode(idAttr) //<p id="p5"></p>
+
+//4. create text value "#5"
+const pText = document.createTextNode("#5")
+
+//5. binding "#5" to <p>
+pElement.appendChild(pText) //<p id="p5">#5</p>
+
+//6. binding <p id="p5">#5</p> to <body>
+const body = document.body
+body.appendChild(pElement) //<body>....<p id="p5">#5</p></body>
