599e198863e8e2cd5b62ddf997ca09f174e5e043
Bearry2626 <satang3223@gmail.com>
2025-10-01 11:49:07 +0700

    update week08 in class

---
+++ week08/manipulatingNode/main.js
@@ -0,0 +1,15 @@
+
+//  3. replace Beef Soup with Pork Soup
+//2.1 get its parent
+const soupUlElement = document.getElementById("soup")
+//2.2 get ref node -Beef Soup
+let refNode = null
+Array.from(soupUlElement.children).forEach((element) => {
+  if (element.textContent.trim() === "Beef Soup") refNode = element
+})
+//2.3 create new node <li class="meat">Pork Soup</li>
+const newNode = document.createElement("li")
+newNode.textContent = "Pork Soup"
+newNode.setAttribute("class", "meat")
+//2.4 replaceChild(newNode, refNode)
+soupUlElement.replaceChild(newNode, refNode)
--- week08/selecttionNodes/main.js
+++ week08/selecttionNodes/main.js
@@ -1,23 +1,22 @@
-// // 1. getElementByName() returns a NodeList data type
-// const fnameElement = document.getElementsByName("fname");
-// console.log(fnameElement.length);
-// fnameElement.forEach((ele) => console.log(ele))
-// fnameElement.children // undefined มันใช้ไม่ได้กับ NodeList
-// fnameElement[0].children // ใช้ได้
-// // console.log(nameAttrCollection);
+//1. getElementsByName(), return NodeList (array-like) data type, implement forEach
+const fnameElements = document.getElementsByName("fname")
+console.log(fnameElements.length) //2
+fnameElements.forEach((ele) => console.log(ele))
 
+//2. getElementsByTagName(), return HTMLCollection (array-like) data type
+const inputElements = document.getElementsByTagName("input")
+Array.from(inputElements).forEach((ele) => console.log(ele))
+ำภำำภกภำ
+//3. getElementsByClassName(), return HTMLCollection (array-like)  data type
+const veganElements = document.getElementsByClassName("vegan")
+Array.from(veganElements).forEach((ele) => console.log(ele))
 
-// // 2. getElementsByTagName(), returns a HTMLCollection data type
-// const inputElements = document.getElementsByTagName("input");
-// Array.from(inputElements).forEach((ele) => console.log(ele)) // forEach() ไม่ได้ใช้กับ HTMLCollection จ้องเพิ่ม array.from() ก่อน  (convert to array first)
-// // 3. gittElementsByClassName(), returns a HTMLCollection data type
-// const veganElements = document.getElementsByClassName("vegan");
-// Array.from(veganElements).forEach((ele) => console.log(ele)) // forEach() ไม่ได้ใช้กับ HTMLCollection จ้องเพิ่ม array.from() ก่อน (convert to array first)
+//4. find the first element of document that has an ID values "appetizer" or "soup"
+const firstElement = document.querySelector("#appetizer, #soup")
+console.log(firstElement)
+//5. find all class "meat" elements under <ul id="appetizer"> or <ul id="soup"> only
 
-// 093 ทัศนัยภูวดล
-// find the first element of doucmen that has an ID values "id appetuzer" or "id soup"
-const firstElement = document.querySelector("#appetizer, #soup");
-console.log(firstElement);
 // find all class "meat" elemet under <ul id = "appetizer"> or <ul id="soup"> only
-const meatElemet = document.querySelectorAll("#appetizer .meat, #soup .meat");
-meatElemet.forEach((ele) => console.log(ele));
+const meatElemet = document.querySelectorAll("#appetizer .meat, #soup .meat")
+meatElemet.forEach((ele) => console.log(ele))
+ 
