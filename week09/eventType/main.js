6795d45cfc4ef90d3a2bd94d3743726d59ca379c
Bearry2626 <satang3223@gmail.com>
2025-10-14 02:34:09 +0700

    add week09 focusEvent and update progression09.txt

--- week09/eventType/main.js
+++ week09/eventType/main.js
@@ -1,6 +1,23 @@
-const message = document.querySelector('#message')
-const display = document.querySelector('p')
-message.addEventListener('change', function () {
-display.textContent = message.value
+const info = document.getElementById('info')
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOM is ready!')
+  const heading = document.createElement('h2')
+  heading.textContent = 'This was added when DOM was ready!'
+  document.body.appendChild(heading)
+  console.log('Heading added now.')
+})
+window.addEventListener('load', () => {
+  console.log('All resources (images, CSS, scripts) are fully loaded!')
+  console.log('Page is fully loaded')
 })
 
+// Window resize event
+window.addEventListener('resize', () => {
+  info.textContent = `Window resized: ${window.innerWidth} x ${window.innerHeight}`
+  console.log("window resize ")
+})
+
+// Window scroll event
+window.addEventListener('scroll', () => {
+    info.textContent = `Scrolled to Y: ${window.scrollY}`
+})
